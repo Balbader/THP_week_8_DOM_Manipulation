ebeab1e109f1a1442e711061890921be9db8c0fb
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-26 20:17:57 +0100

    initial

---
+++ script_parcours_DOM.js
@@ -0,0 +1,7 @@
+// Total number of Paragraph
+let totalNumberOfParagraph = document.getElementsByTagName("p").length;
+console.log(totalNumberOfParagraph); // Output: 20
+
+// console.log the content of element with ID 'coucou'
+let coucouContent = document.getElementById("coucou").innerHTML;
+console.log(coucouContent);
