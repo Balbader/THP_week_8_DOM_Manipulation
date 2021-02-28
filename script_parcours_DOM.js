cec96275e6bdb2353861b25a2472e30cad4c2a9d
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-28 14:15:40 +0100

    update scritpe

--- script_parcours_DOM.js
+++ script_parcours_DOM.js
@@ -5,3 +5,7 @@ console.log(totalNumberOfParagraph); // Output: 20
 // console.log the content of element with ID 'coucou'
 let coucouContent = document.getElementById("coucou").innerHTML;
 console.log(coucouContent);
+
+// URL of 3rd element:
+const thirdElemUrl = document.querySelectorAll("a")[2].getAttribute("href");
+console.log(thirdElemUrl);
