ab145155fc5913f65843b80baba09414531f864f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-02-28 14:23:56 +0100

    update script

--- script_parcours_DOM.js
+++ script_parcours_DOM.js
@@ -1,11 +1,15 @@
 // Total number of Paragraph
-let totalNumberOfParagraph = document.getElementsByTagName("p").length;
+const totalNumberOfParagraph = document.getElementsByTagName("p").length;
 console.log(totalNumberOfParagraph); // Output: 20
 
 // console.log the content of element with ID 'coucou'
-let coucouContent = document.getElementById("coucou").innerHTML;
+const coucouContent = document.getElementById("coucou").innerHTML;
 console.log(coucouContent);
 
 // URL of 3rd element:
 const thirdElemUrl = document.querySelectorAll("a")[2].getAttribute("href");
 console.log(thirdElemUrl);
+
+// Total number of element with class name: compte-moi
+const compteMoi = document.getElementsByClassName("compte-moi").length;
+console.log(compteMoi);
